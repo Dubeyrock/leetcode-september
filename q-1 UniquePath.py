ba84c56a3a4394b7fc71cde2c88b6017359d497f
shivam Dubey <96882359+Dubeyrock@users.noreply.github.com>
2023-09-03 10:12:29 +0530

    Create q-1 UniquePath.py

---
+++ q-1 UniquePath.py
@@ -0,0 +1,22 @@
+class Solution:
+    def uniquePaths(self, m : int, n: int) ->int:
+        dp = [[1 if i == 0 or j == 0 else 0 for j in range(n)] for i in range(m)]
+        for i in range(1,m):
+            for j in range(1,n):
+
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+
+
+# memoization technique 
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        curr_row = [1] * n
+        prev_row = [1] * n
+
+        for i in range(1,m):
+            for j in range(1,n):
+                curr_row[j] = curr_row[j-1] + prev_row[j]
+            curr_row, prev_row = prev_row, curr_row
+        return prev_row[-1]
